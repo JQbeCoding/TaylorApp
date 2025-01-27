e8939e9b361ae5c369389540ac472e76ae7b9974
Ja'Quis <148560046+JQbeCoding@users.noreply.github.com>
2025-01-27 12:52:52 -0500

    Update taylorAppMain.java

--- taylorAppMain.java
+++ taylorAppMain.java
@@ -21,20 +21,7 @@ public class taylorAppMain {
 			switch(input.toLowerCase()){
 				case "e":
 					System.out.println("WELCOME!!!");
-					System.out.println("Enter your pet type");
-					
-					//User enters the type of pet
-					String type = userInput.nextLine();
-					
-					System.out.println("Enter your pet's name");
-					
-					//User enters their pet name
-					String name = userInput.nextLine();
-					
-					Pet pet = new Pet(type, name);
-					//Displays pet type and name
-					System.out.println(pet.toString());
-					
+					choosePet();
 					return;
 				
 				case "q":
@@ -57,6 +44,62 @@ public class taylorAppMain {
 		System.out.println("******Welcome to the Taylor App!!*******");
 		System.out.println(" Press [E] To Enter  Press [Q] to Quit  ");
 	}
-}
 	
-
+	/**
+	 * User selection for their preferred pet
+	 */
+	public static void choosePet() {
+		try (Scanner kbd = new Scanner(System.in)) {
+			
+			Pet userPet = new Pet();
+			//User chooses/enters the type of pet
+			System.out.println("Type a letter or enter the type of pet you want:");
+			System.out.println("[D]og");
+			System.out.println("[C]at");
+			System.out.println("[F]ish");
+			System.out.println("[E]nter a custom type");
+			
+			String input = kbd.nextLine();
+			boolean entered = false;
+			
+			while(!entered) {
+			switch(input.toLowerCase()) {
+			
+			case "d":
+				userPet.setPetType("Dog");
+				entered = true;
+				break;
+			
+			case "c":
+				userPet.setPetType("Cat");
+				entered = true;
+				break;
+			
+			case "f":
+				userPet.setPetType("Fish");
+				entered = true;
+				break;
+			
+			case "e":
+				System.out.println("Enter your pet type:");
+				String petType = kbd.nextLine();
+				userPet.setPetType(petType);
+				entered = true;
+				break;
+				
+			default:
+				System.out.println("Invalid input. Please try again.");
+			}
+		}
+			
+			System.out.println("Enter your pet name: ");
+			//User enters their pet name
+			String petName = kbd.nextLine();
+			userPet.setPetName(petName);
+			
+			;
+			//Displays pet type and name
+			System.out.println(userPet.toString());
+		}	
+	}
+}
